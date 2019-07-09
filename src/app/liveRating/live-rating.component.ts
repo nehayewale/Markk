0b7aa4a2bc261f8840e4066cd81cde13d89a40b5
nehayewale <37070492+nehayewale@users.noreply.github.com>
2019-07-09 10:05:30 +0530

    Add files via upload

---
+++ src/app/liveRating/live-rating.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-live-rating',
+  templateUrl: './live-rating.component.html',
+  styleUrls: ['./live-rating.component.css']
+})
+export class LiveRatingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
